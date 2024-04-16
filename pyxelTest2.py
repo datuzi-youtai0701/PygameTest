19d4063f4881363f216b5831b7ed87e32efc010b
datuzi-youtai0701 <166999038+datuzi-youtai0701@users.noreply.github.com>
2024-04-17 00:01:08 +0900

    Update pyxelTest2.py

--- pyxelTest2.py
+++ pyxelTest2.py
@@ -1,15 +1,29 @@
 
 import pyxel
 #コメントのテスト
-pyxel.init(150, 150)
-pyxel.mouse(True)
+class App:
+    def __init_(self):
+        #ディスプレイの初期化
+        pyxel.init(150, 150)
+        
+        #マウスカーソルを表示
+        pyxel.mouse(True)
 
-def update():
+        #アプリケーションの開始
+        pyxel.run(self.update, self.draw)
+        
+#開始時に呼ばれる関数
+def update(self):
     if pyxel.btnp(pyxel.KEY_Q):
         pyxel.quit()
-
-def draw():
+        
+#起動時に呼ばれる描画関数
+def draw(self):
     pyxel.cls(0)
     pyxel.rect(pyxel.mouse_x, pyxel.mouse_y, 5, 5, 6)
 
-pyxel.run(update, draw)
+App()
+
+
+
+
