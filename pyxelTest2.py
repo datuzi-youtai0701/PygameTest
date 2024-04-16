f3796023507449b8c8e3ea34b57f599583e5cac2
datuzi-youtai0701 <166999038+datuzi-youtai0701@users.noreply.github.com>
2024-04-17 01:22:37 +0900

    Add files via upload

--- pyxelTest2.py
+++ pyxelTest2.py
@@ -1,42 +1,68 @@
 
 import pyxel
 #コメントのテスト
+
+
 class App:
-    def __init_(self):
+
+    screenX = 150
+    screenY = 150
+    
+
+    
+    def __init__(self):
+        
         #ディスプレイの初期化
-        pyxel.init(150, 150)
+        pyxel.init(self.screenX, self.screenY, title="pyxelTest2.py", fps=10 )
         
         #マウスカーソルを表示
         pyxel.mouse(True)
 
         #アプリケーションの開始
         pyxel.run(self.update, self.draw)
+
+
         
-#開始時に呼ばれる関数
-def update(self):
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+    #開始時に呼ばれる関数
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+            
+        self.x = pyxel.rndi(15, 135)
+        self.y = pyxel.rndi(15, 135)
+        self.r = pyxel.rndi(5, 15)
+        self.col = pyxel.rndi(5, 15)
         
-#起動時に呼ばれる描画関数
-def draw(self):
-    pyxel.cls(0)
-    pyxel.rect(pyxel.mouse_x, pyxel.mouse_y, 5, 5, 6)
+        
+    #起動時に呼ばれる描画関数
+    def draw(self):
+        self.cls()
+        self.rectMouse(pyxel.mouse_x, pyxel.mouse_y, 30, 30, 6)
+        self.textMousePosition(16,130)
+        self.drawCircle()
 
-#マウスの座標を表示
-def textMousePosition(self,x,y)
-    mousePositionText = (
-        f"Current mouse position is({pyxel.mouse_x},{pyxel.mouse_y})
-        )
-    pyxel.text(x, y, mousePositionText, 9)
+    def cls(self):
+        pyxel.cls(0)
 
-App()
-
-
-
-
-
-
-
+    def rectMouse(self,x,y,w,h,c):
+        pyxel.rect(x,y,w,h,c)
+        
+        
+    #マウスの座標を表示
+    def textMousePosition(self,x,y):
+        mousePositionText = (
+            f"Current mouse position is({pyxel.mouse_x},{pyxel.mouse_y})"
+            )
+        pyxel.text(x, y, mousePositionText, 9)
 
 
-
+    
+    def drawCircle(self):
+        
+        #for _ in range(1):
+        
+            
+            pyxel.circ(self.x,self.y,self.r,self.col)
+   
+    
+App()   
