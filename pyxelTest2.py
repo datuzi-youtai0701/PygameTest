c15f688eb15be79a319b6f747926fb08ec198b3e
datuzi-youtai0701 <166999038+datuzi-youtai0701@users.noreply.github.com>
2024-04-18 00:19:03 +0900

    Add files via upload

--- pyxelTest2.py
+++ pyxelTest2.py
@@ -2,25 +2,38 @@
 import pyxel
 #コメントのテスト
 
+class player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y= y
+        
+
+
+
+
 
 class App:
-
-    
-    
-
     
     def __init__(self):
-        screenX = 150
-        screenY = 150      
+        self.screenX = 150
+        self.screenY = 150
+
+        
+        self.player = player(16, 16)
         
         #ディスプレイの初期化
-        pyxel.init(self.screenX, self.screenY, title="pyxelTest2.py", fps=10 )
+        pyxel.init(self.screenX, self.screenY, title="pyxelTest2.py", fps=20 )
         
         #マウスカーソルを表示
         pyxel.mouse(True)
 
         #アプリケーションの開始
         pyxel.run(self.update, self.draw)
+
+        #経過フレーム数でTrueにするトグル
+        self.frameCounterEnabled = False
+
+        
 
 
         
@@ -33,14 +46,27 @@ class App:
         self.y = pyxel.rndi(15, 135)
         self.r = pyxel.rndi(5, 15)
         self.col = pyxel.rndi(5, 15)
+
+        #経過フレーム数でTrueにするトグルをTrueにする
+        self.frameCounterEnabled = (pyxel.frame_count % 10) == 0
+
+        if pyxel.btnp(pyxel.KEY_LEFT):
+            self.player.x -= 1
+        elif pyxel.btnp(pyxel.KEY_RIGHT):
+            self.player.x += 1
+        
         
         
     #起動時に呼ばれる描画関数
     def draw(self):
+        
         self.cls()
         self.rectMouse(pyxel.mouse_x, pyxel.mouse_y, 30, 30, 6)
         self.textMousePosition(16,130)
+        #if self.frameCounterEnabled:
         self.drawCircle()
+
+        pyxel.rect(self.player.x, self.player.y, 20, 20, 4)
 
     def cls(self):
         pyxel.cls(0)
@@ -60,10 +86,9 @@ class App:
     
     def drawCircle(self):
         
-        #for _ in range(1):
-        
             
-            pyxel.circ(self.x,self.y,self.r,self.col)
+            #for _ in range(1):
+                pyxel.circ(self.x,self.y,self.r,self.col)
    
     
 App()   
